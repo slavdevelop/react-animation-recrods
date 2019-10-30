3b0cff2928d0ebbf3613e3d217cea717f87fc532
Slavi Shtipkov <development@shtipkov.com>
2019-10-30 04:16:33 +0200

    Fix animation and add animation on create message

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const ListCard = ({ item: { text } }) => {
   return (
-    <div className="col-md-7">
-      <div className="card my-3 p-3 shadow-sm text-center">
-        <div className="card-body">
-          <h4 className="card-title">{text}</h4>
-        </div>
+    <div className="card my-3 p-3 shadow-sm text-center">
+      <div className="card-body">
+        <h4 className="card-title">{text}</h4>
       </div>
     </div>
   );
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useTransition, animated } from 'react-spring';
 
 import ListCard from './ListCard';
 
 const ListContainer = ({ items }) => {
+  const transition = useTransition(items, items => items.id, {
+    from: { opacity: 0, marginLeft: -100, marginRight: 100 },
+    enter: { opacity: 1, marginLeft: 0, marginRight: 0 }
+  });
+
   return (
     <>
-      {items.map(item => (
-        <ListCard key={item.id} item={item} />
+      {transition.map(({ item, key, props }) => (
+        <animated.div key={key} style={props} className="col-md-7">
+          <ListCard item={item} />
+        </animated.div>
       ))}
     </>
   );
